f1f9bfb0808462eaf4cc58341f8a640ba51331ce
phihd <phictin@gmail.com>
2023-03-04 12:37:59 +0700

    add font

--- front-end/src/index.js
+++ front-end/src/index.js
@@ -4,6 +4,8 @@ import App from './App';
 
 ReactDOM.render(
   <React.StrictMode>
+    <link rel="preconnect" href="https://fonts.gstatic.com" />
+    <link href="https://fonts.googleapis.com/css2?family=Playfair+Display&display=swap" rel="stylesheet" />
     <App />
   </React.StrictMode>,
   document.getElementById('root')
